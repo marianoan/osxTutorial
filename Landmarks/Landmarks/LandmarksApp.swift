407353998af394cafafd1058f89cb0be6b2aa6d6
gonzalez mariano <gonzalezmariano@sgo-1q9f1wgpq.bcg.com>
2023-06-10 18:57:17 -0400

    Creating views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by gonzalez mariano on 09-06-23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
