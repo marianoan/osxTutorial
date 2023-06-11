32e9ac32f7d3f75756299297a9eee63f567a41d6
gonzalez mariano <gonzalezmariano@sgo-1q9f1wgpq.bcg.com>
2023-06-10 23:20:53 -0400

    Lists and navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by gonzalez mariano on 09-06-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
